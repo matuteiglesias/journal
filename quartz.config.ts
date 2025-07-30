3484cc1187ebf79ffa32df49ff5956d18c44e9b3
matuteiglesias <matuteiglesias@gmail.com>
2025-07-30 10:03:34 -0300

    updated config

--- quartz.config.ts
+++ quartz.config.ts
@@ -10,7 +10,7 @@ const config: QuartzConfig = {
   configuration: {
     pageTitle: "M.I. Journal",
     pageTitleSuffix: "",
-    enableSPA: true,
+    enableSPA: false,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
